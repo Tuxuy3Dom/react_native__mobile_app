88fe75a1e0c745699e1f7ac3961520f64b02b988
Tuxuy3Dom <ihor.yaremchuk2120@gmail.com>
2023-07-19 23:56:59 +0200

    Update list card company name

--- my-app/hook/useFetch.js
+++ my-app/hook/useFetch.js
@@ -1,8 +1,50 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const useFetch = () => {
+const useFetch = (endpoint, query) => {
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
+
+    const axios = require('axios');
+
+    const options = {
+        method: 'GET',
+        url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+        headers: {
+            'X-RapidAPI-Key': '4337205ed9msha081767daec1b4fp1b77afjsn1c5e9424abde',
+            'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+        },
+        params: { ...query },
+    };
+
+    const fetchData = async () => {
+        setIsLoading(true);
+
+        try {
+            const response = await axios.request(options);
+            setData(response.data.data);
+            setIsLoading(false);
+            console.log(response.data);
+        } catch (error) {
+            setError(error);
+            alert('Tam jest błąd');
+            console.error(error);
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const refetch = () => {
+        setIsLoading(true);
+        fetchData();
+    }
+
+    return { data, isLoading, error, refetch };
 }
+
+export default useFetch;
